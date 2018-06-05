c7ec19cdea10f83c3af57bf1a005a198f936fbba
annaboyatyuk <annaboyatyuk@gmail.com>
2018-06-04 23:08:42 -0700

    ll insertions

---
+++ linked-lists/05-linked-lists/__test__/05-linked-list.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let SinglyLinkedList = require('../lib/05-linked-list');
+
+describe('Singly Linked List', () => {
+
+  it('', () => {
+    expect().toBe();
+  });
+
+
+});
---
+++ linked-lists/ll_insertions/ll_insertions.js
@@ -0,0 +1,59 @@
+
+
+
+
+// linked lists
+
+
+// for the lab
+
+
+// append(value) {
+
+//   if(!this.root) {
+//     this.root = new ListNode(value);
+//   }
+//   else {
+
+//     let node = this.root;
+
+//     while(node.next) {
+//       node = node.next;
+//     }
+
+
+//     node.next = new ListNode(value);
+
+//   }
+// }
+
+
+
+// prepend(value) {
+
+//   const newNode = new ListNode(value);
+  
+//   if(!this.root) {
+
+//     this.root = newNode;
+//   } else {
+    
+//     newNode.next = this.root;
+//     this.root = newNode;
+//   };
+// };
+
+
+// insertBefore(value, newValue) {
+
+//   let node = new ListNode(newValue);
+
+//   let current = this.root;
+
+//   while(current) {
+    
+//   }
+
+// }
+
+
